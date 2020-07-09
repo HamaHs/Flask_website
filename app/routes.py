0f9cc3c3d2c8ca97870054b0ba826298daca4d73
HamaHs <Lusuy1995@gmail.com>
2020-07-09 17:21:27 +0300

    add new functions

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template, redirect, flash
+from flask_login import current_user, login_user, logout_user
 
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -27,17 +29,29 @@ def about():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_form():
+    if current_user == User.is_authenticated:
+        return redirect('/main')
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect('/login')
+        login_user(user, remember=form.remember_me.data)
         return redirect('/main')
     return render_template('login.html', title='Sign in', form=form)
 
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/main')
+
+
 @app.errorhandler(404)
-def not_found(error):
+def not_found():
     return render_template('not_found_404.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
